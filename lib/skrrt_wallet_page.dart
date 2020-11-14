43656e116568808cc30a8e040f65639c9cd4ab1f
chololong <jeremykandt2@gmail.com>
2020-11-14 18:57:30 +0800

    sidebar and navigations

---
+++ lib/skrrt_wallet_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter/material.dart';
+import 'sidebar_page.dart';
+
+class SkrrtWallet extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+          onPressed: (){
+            _scaffoldKey.currentState.openDrawer();
+          },
+        ),
+      ),
+      drawer: SafeArea(
+        child: SideBar(),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+            "SKRRT Wallet",
+            style: TextStyle(fontWeight: FontWeight.w900,fontSize: 28),),
+        ),
+      ),
+
+    );
+  }
+}
