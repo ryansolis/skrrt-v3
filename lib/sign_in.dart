04e08a05cfba17022eaf6c58ab5d78041c5ac255
renceobrero <renceobrero@gmail.com>
2020-11-30 10:12:23 +0800

    Updated Sign In: -made it responsive
    Updated Updated Sign Up:
    -Complete with Verification
    
    Updated New_User dart:
    -complete steps
    
    Wala pakos Homepage AAAAAAA

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'sign_up.dart';
-//import 'home_page.dart';
 import 'new_user.dart';
 
 class SignIn extends StatefulWidget {
@@ -12,7 +11,7 @@ class _SignInState extends State<SignIn> {
   String _username;
   String _password;
 
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildUsername(){
     return TextFormField(
@@ -33,19 +32,19 @@ class _SignInState extends State<SignIn> {
       ),
       style: TextStyle(color: Color.fromARGB(255, 0x00, 0xA8, 0xE5),),
       keyboardType: TextInputType.text,
-      /*
-      validator: (String value){
-        if(value.isEmpty){
-          return 'Username is Required.';
+      validator: (username){
+        Pattern pattern =
+            r'^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$';
+        RegExp regex = new RegExp(pattern);
+        if (username.isEmpty) {
+          return 'Username is required.';
         }
-        if(!RegExp(r'^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$').hasMatch(value)){
-          return 'Please enter a valid Username.';
-        }
-        else return null;
+        else if (!regex.hasMatch(username))
+          return 'Invalid Username.';
+        else
+          return null;
       },
-      onSaved: (String value) {
-        _username = value;
-      },*/
+      onSaved: (username)=> _username = username,
     );
   }
 
@@ -69,16 +68,19 @@ class _SignInState extends State<SignIn> {
       style: TextStyle(color: Color.fromARGB(255, 0x00, 0xA8, 0xE5),),
       keyboardType: TextInputType.text,
       obscureText: true,
-      /*
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Password is Required.';
+      validator: (password){
+        Pattern pattern =
+            r'^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$';
+        RegExp regex = new RegExp(pattern);
+        if (password.isEmpty) {
+          return 'Password is required.';
         }
-        else return null;
+        else if (!regex.hasMatch(password))
+          return 'Invalid Password.';
+        else
+          return null;
       },
-      onSaved: (String value) {
-        _password = value;
-      },*/
+      onSaved: (password)=> _password = password,
     );
   }
 
@@ -120,57 +122,54 @@ class _SignInState extends State<SignIn> {
                             )
                         ),
                         Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                _buildUsername(),
-                                SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
-                                _buildPassword(),
-                                SizedBox(height: MediaQuery.of(context).size.height * 0.05,),
-                                RaisedButton(
-                                    padding: EdgeInsets.all(12.0),
-                                    shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(50.0)),
-                                    color: Color(0xff00A8E5),
-                                    disabledColor: Colors.blue,//add this to your code
-                                    child: Text(''
-                                        'LOG IN',
+                            child: Form(
+                              key: _formKey,
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  children: [
+                                  _buildUsername(),
+                                  SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
+                                  _buildPassword(),
+                                  SizedBox(height: MediaQuery.of(context).size.height * 0.05,),
+                                  RaisedButton(
+                                      padding: EdgeInsets.all(12.0),
+                                      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(50.0)),
+                                      color: Color(0xff00A8E5),
+                                      disabledColor: Colors.blue,//add this to your code
+                                      child: Text(''
+                                          'LOG IN',
+                                        style: TextStyle(
+                                          fontFamily: 'Montserrat',
+                                          color:Colors.white,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        if(_formKey.currentState.validate()){
+                                          _formKey.currentState.save();
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => NewUser()),
+                                          );
+                                        }
+
+                                      }
+                                  ),
+                                  SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
+                                  Center(
+                                    child: Text(
+                                      'Forgot Password',
                                       style: TextStyle(
-                                        fontFamily: 'Montserrat',
-                                        color:Colors.white,
-                                        fontSize: 16,
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 16.0,
+                                        decoration: TextDecoration.underline,
+                                        letterSpacing: 1.0,
+                                        color: Color.fromARGB(255, 0x00, 0xA8, 0xE5),
                                       ),
                                     ),
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => NewUser()),
-                                      );
-                                      /*
-                                    if(!formKey.currentState.validate()){
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => LaunchPage()),
-                                      )
-                                    }
-                                    else{
-                                      formKey.currentState.save()
-                                    }*/
-                                    }
-                                ),
-                                SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
-                                Center(
-                                  child: Text(
-                                    'Forgot Password',
-                                    style: TextStyle(
-                                      fontFamily: 'Quicksand',
-                                      fontSize: 16.0,
-                                      decoration: TextDecoration.underline,
-                                      letterSpacing: 1.0,
-                                      color: Color.fromARGB(255, 0x00, 0xA8, 0xE5),
-                                    ),
-                                  ),
-                                )
-                              ],
+                                  )
+                                ],
+                              )
                             )
                         ),
                         //SizedBox( height: MediaQuery.of(context).size.height * 0.1,),
